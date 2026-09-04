2a8cfd85607b7f12e36d3ecf34b4452ba6ba6767
zogaro <zogaro.pro@gmail.com>
2026-09-05 01:40:23 +0200

    Update setup.py .aris

--- plugin.video.aris/setup.py
+++ plugin.video.aris/setup.py
@@ -1,24 +1,19 @@
 # -*- coding: utf-8 -*-
-# https://github.com/Kodi-vStream/venom-xbmc-addons
+# Aris (fork de vStream) - https://github.com/zogaro/aris-xbmc-addons
 from setuptools import setup, find_packages
-
 setup(
-    name='vStream',
+    name='Aris',
     version='0.1',
     packages=find_packages(),
-
     # Declare your packages' dependencies here, for eg:
     install_requires=['foo>=3'],
-
     # Fill in these to make your Egg ready for upload to
     # PyPI
-    author='Venom',
+    author='zogaro',
     author_email='',
-
     summary='Just another Python package for the cheese shop',
     url='',
     license='',
     long_description='Long description of the package',
-
     # could also include long_description, download_url, classifiers, etc.
 )
