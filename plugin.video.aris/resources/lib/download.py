d85b94657e44d4fc13e2ea50d7fbd6668a62f76a
zogaro <zogaro.pro@gmail.com>
2026-09-05 03:10:24 +0200

    Update download.py aris

--- plugin.video.aris/resources/lib/download.py
+++ plugin.video.aris/resources/lib/download.py
@@ -332,7 +332,7 @@ class cDownload:
         sPluginPath = cPluginHandler().getPluginPath()
         sItemUrl = '%s?site=%s&function=%s&title=%s' % (sPluginPath, SITE_IDENTIFIER, 'StartDownloadList', 'title')
         item = xbmcgui.ListItem('Démarrer la liste')
-        item.setArt({'icon':'special://home/addons/plugin.video.vstream/resources/art/download.png'})
+        item.setArt({'icon':'special://home/addons/plugin.video.aris/resources/art/download.png'})
         xbmcplugin.addDirectoryItem(sPluginHandle, sItemUrl, item, isFolder=False)
 
         oGui = cGui()
