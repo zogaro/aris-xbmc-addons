5c849fe4a2a56ec72340ded28d66ce0c21a78a2e
zogaro <zogaro.pro@gmail.com>
2026-09-05 04:22:02 +0200

    Update menuAddBookmark.py aris

--- plugin.video.aris/resources/lib/gui/menuAddBookmark.py
+++ plugin.video.aris/resources/lib/gui/menuAddBookmark.py
@@ -1,2 +1,2 @@
 import xbmc
-xbmc.executebuiltin("RunPlugin(plugin://plugin.video.vstream/?site=cFav&function=setBookmark)", True)
+xbmc.executebuiltin("RunPlugin(plugin://plugin.video.aris/?site=cFav&function=setBookmark)", True)
